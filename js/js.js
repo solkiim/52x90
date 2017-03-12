ca1f0ddf6a1edcf242b8d91e2f970eb5cf29c0ab
Sol Kim <solkim813@gmail.com>
2017-03-12 18:23:44 -0400

    dots and js progress

--- js/js.js
+++ js/js.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
 	// iterate through and make original empty days
 	var totalstr = '';
-	for (var year = 0; year < 90; year++) {
-		var yearstr = '<div class="year"';
+	for (var year = 1; year <= 90; year++) {
+		var yearstr = '<div class="year">';
 		
-		for (var week = 0; week < 52; week++) {
+		// if decade marking
+		if (year % 10 == 1) {
+			//yearstr += '<p class="decade">' + year + '</p>'
+			yearstr += '<hr style="height:8px; margin:0; visibility:hidden;" />'
+		}
+		
+		for (var week = 1; week <= 52; week++) {
 			yearstr += '<div class="week unfilled"></div>';
 		}
 		
@@ -12,9 +18,25 @@ $(document).ready(function(){
 	}
 	
 	$("#total").html(totalstr);
+	
+	fill(1017);
 });
 
 
 function fill(count) {
-	
+//	var filler = setInterval(function(){
+//		// done filling
+//		if (count === -1) clearInterval(filler)
+//		
+//		
+//		alert("Hello");
+//	}, 100);
+	var wholeyears = Math.floor(count / 52);
+	var remainderweeks = count % 52;
+	for (var year = 1; year <= wholeyears; year++) {
+		$("#total .year:nth-child(" + year + ") .week").removeClass("unfilled").addClass("filled");
+	}
+	for (var week = 1; week <= remainderweeks; week++) {
+		$("#total .year:nth-child(" + (wholeyears + 1) + ") .week:nth-child(" + week + ")").removeClass("unfilled").addClass("filled");
+	}
 }
