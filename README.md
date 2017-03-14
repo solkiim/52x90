ebe7e8fff5b5d4c4de8685cbe5be2c8319b76bd3
Sol Kim <solkim813@gmail.com>
2017-03-13 23:40:54 -0400

    totally new

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# 52x90
-your life in weeks
+
+Page Transitions
+=========
+
+A showcase collection of various page transition effects using CSS animations.
+
+[article on Codrops](http://tympanus.net/codrops/?p=15001)
+
+[demo](http://tympanus.net/Development/PageTransitions/)
+
+[LICENSING & TERMS OF USE](http://tympanus.net/codrops/licensing/)
--- js/js.js
+++ js/js.js
@@ -1,14 +1,23 @@
+var birthday = new Date("August 13, 1997 00:00:00");
+
 $(document).ready(function(){
+	makeWeek(1017);
+});
+
+
+
+/* ------------------------- FILL IN WEEKS ------------------------- */
+function makeWeek(weekcount) {
 	// iterate through and make original empty days
 	var totalstr = '';
 	for (var year = 1; year <= 90; year++) {
 		var yearstr = '<div class="year">';
 		
-		// if decade marking
-		if (year % 10 == 1) {
-			//yearstr += '<p class="decade">' + year + '</p>'
-			yearstr += '<hr style="height:8px; margin:0; visibility:hidden;" />'
-		}
+//		// if decade marking
+//		if (year % 10 == 1) {
+//			//yearstr += '<p class="decade">' + year + '</p>'
+//			yearstr += '<hr style="height:8px; margin:0; visibility:hidden;" />'
+//		}
 		
 		for (var week = 1; week <= 52; week++) {
 			yearstr += '<div class="week unfilled"></div>';
@@ -19,20 +28,12 @@ $(document).ready(function(){
 	
 	$("#total").html(totalstr);
 	
-	fill(1017);
-});
+	fillWeek(weekcount);
+}
 
-
-function fill(count) {
-//	var filler = setInterval(function(){
-//		// done filling
-//		if (count === -1) clearInterval(filler)
-//		
-//		
-//		alert("Hello");
-//	}, 100);
-	var wholeyears = Math.floor(count / 52);
-	var remainderweeks = count % 52;
+function fillWeek(weekcount) {
+	var wholeyears = Math.floor(weekcount / 52);
+	var remainderweeks = weekcount % 52;
 	for (var year = 1; year <= wholeyears; year++) {
 		$("#total .year:nth-child(" + year + ") .week").removeClass("unfilled").addClass("filled");
 	}
